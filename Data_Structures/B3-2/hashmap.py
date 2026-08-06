f7bf2a00af77cf7e1c85ad5a7c1bed14c2826d87
sonjehyun123-maker <sonjehyun123@gmail.com>
2026-08-06 13:42:19 +0900

    B3-2/hashmap_버킷변경

--- Data_Structures/B3-2/hashmap.py
+++ Data_Structures/B3-2/hashmap.py
@@ -1,5 +1,5 @@
 class Node:
-    """해시맵 버킷 내 체이닝을 위해 Commit을 감싸는 연결 노드."""
+    """체이닝용 노드."""
 
     def __init__(self, commit):
         self.commit = commit
@@ -7,30 +7,26 @@ class Node:
 
 
 class HashMap:
-    """hash를 키로 Commit을 저장하는 체이닝 방식 해시맵. 버킷 개수는 고정(리해시 미구현)."""
+    """앞 2자리(16^2=256가지)를 버킷 번호로 직접 쓰는 해시맵.
+    SHA-1 출력은 이미 균등하게 흩어지므로 별도 해시 함수(djb2 등) 없이 접두사를 그대로 씀."""
 
-    def __init__(self, bucket_count=16):
+    def __init__(self, bucket_count=256):
         self.bucket_count = bucket_count
         self.buckets = [None] * bucket_count
 
     def _hash_index(self, key):
-        """문자열 key를 djb2 스타일로 해싱해 버킷 인덱스를 계산한다."""
-        hash_value = 0
-        for char in key:
-            hash_value = hash_value * 33 + ord(char)
-        return hash_value % self.bucket_count
+        """key의 앞 2자리(16진수)를 그대로 버킷 번호로 쓴다."""
+        return int(key[:2], 16)
 
     def put(self, commit):
-        """Commit을 해시맵에 저장한다. 같은 hash가 이미 있으면 commit.hash가 유일해질 때까지 extend_hash()로 재시도한다."""
-        while self.get(commit.hash) is not None:
-            commit.extend_hash()
+        """Commit을 저장한다."""
         index = self._hash_index(commit.hash)
         new_node = Node(commit)
         new_node.next = self.buckets[index]
         self.buckets[index] = new_node
 
     def get(self, key):
-        """hash로 저장된 Commit을 조회한다. 없으면 None을 반환한다."""
+        """전체 hash로 Commit을 조회한다."""
         index = self._hash_index(key)
         current = self.buckets[index]
         while current is not None:
@@ -38,3 +34,24 @@ class HashMap:
                 return current.commit
             current = current.next
         return None
+
+    def find_by_prefix(self, prefix):
+        """짧은 접두사로 시작하는 모든 Commit을 찾는다.
+        접두사가 2자리 이상이면 해당 버킷 하나만 확인(빠름), 아니면 전체를 훑는다."""
+        if len(prefix) >= 2:
+            try:
+                index = self._hash_index(prefix)
+            except ValueError:
+                return []
+            buckets_to_check = [self.buckets[index]]
+        else:
+            buckets_to_check = self.buckets
+
+        matches = []
+        for bucket in buckets_to_check:
+            node = bucket
+            while node is not None:
+                if node.commit.hash.startswith(prefix):
+                    matches.append(node.commit)
+                node = node.next
+        return matches
